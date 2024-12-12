84318b39ba5a5c41c5be86fada03b9572dcea550
Anioke sebastian <aniokechukwudi7@gmail.com>
2024-12-12 20:12:14 +0100

    add showtoast and shadcn dialog

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import toast from "react-hot-toast";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const showToast = {
+	message: (message: string) => {
+		toast.remove();
+		toast(message);
+	},
+	success: (message: string) => {
+		toast.remove();
+		toast.success(message);
+	},
+	error: (message: string) => {
+		toast.remove();
+		toast.error(message);
+	},
+	loading: (message: string) => {
+		toast.remove();
+		toast.loading(message);
+	},
+};
